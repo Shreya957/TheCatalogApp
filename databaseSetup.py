3528da5badc47af713c25231e52ac1a6aad6664a
Shreya957 <39702794+Shreya957@users.noreply.github.com>
2019-03-08 22:24:20 +0530

    Add files via upload

---
+++ databaseSetup.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base= declarative_base()
+
+class User(Base):
+
+    __tablename__= 'user'
+  
+    name= Column(String(80), nullable = False)
+    id = Column(Integer , primary_key = True)
+    email = Column(String(80), nullable = False)
+
+class Category(Base):
+
+    __tablename__ = 'category'
+
+    name= Column(String(80), nullable = False)
+    id = Column(Integer , primary_key = True)
+  
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+class Item(Base):
+
+    __tablename__ = 'item'
+
+    name= Column(String(80), nullable = False)
+    id = Column(Integer , primary_key = True)
+    description = Column(String(250))
+    creation_date = Column(DateTime, default=datetime.datetime.utcnow)
+    cat_id = Column(Integer, ForeignKey('category.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+  
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
+  
+engine = create_engine('sqlite:///CatalogApplication.db')
+
+Base.metadata.create_all(engine)
