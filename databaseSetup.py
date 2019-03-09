78b3740239443bda0e59d7b6cc91b692f6656345
Shreya957 <39702794+Shreya957@users.noreply.github.com>
2019-03-09 13:15:40 +0530

    modified according to pep8
    modified according to pep8

--- databaseSetup.py
+++ databaseSetup.py
@@ -7,23 +7,21 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-Base= declarative_base()
+Base = declarative_base()
+
 
 class User(Base):
+    __tablename__ = 'user'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(80), nullable=False)
 
-    __tablename__= 'user'
-  
-    name= Column(String(80), nullable = False)
-    id = Column(Integer , primary_key = True)
-    email = Column(String(80), nullable = False)
 
 class Category(Base):
-
     __tablename__ = 'category'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
 
-    name= Column(String(80), nullable = False)
-    id = Column(Integer , primary_key = True)
-  
     @property
     def serialize(self):
         return {
@@ -31,18 +29,17 @@ class Category(Base):
             'id': self.id,
         }
 
+
 class Item(Base):
-
     __tablename__ = 'item'
-
-    name= Column(String(80), nullable = False)
-    id = Column(Integer , primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     creation_date = Column(DateTime, default=datetime.datetime.utcnow)
     cat_id = Column(Integer, ForeignKey('category.id'))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
-  
+
     @property
     def serialize(self):
         return {
@@ -50,7 +47,7 @@ class Item(Base):
             'description': self.description,
             'id': self.id,
         }
-  
+
 engine = create_engine('sqlite:///CatalogApplication.db')
 
 Base.metadata.create_all(engine)
